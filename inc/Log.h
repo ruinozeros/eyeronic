634290b5a0c511858a90f7cfb5aba8a0b174b0c7
Thomas Ruess <rui.nozeros@posteo.at>
2019-05-27 22:33:58 +0200

    inline logging functions

--- inc/Log.h
+++ inc/Log.h
@@ -14,14 +14,14 @@
 
 #include <Defines.h>
 
-void initLog() {
+inline void initLog() {
 	openlog(SYSLOG_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 
 	syslog(LOG_NOTICE, "Program started by User %s (UID=%d)", getenv("USER"),
 			getuid());
 }
 
-void log(LogLevel level, const char* message) {
+inline void log(LogLevel level, const char* message) {
 	switch (level) {
 	case INFO:
 		syslog(LOG_INFO, message);
--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,55 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
-#include <syslog.h>
 #include <string.h>
 #include <signal.h>
-#include <syslog.h>
 #include <iostream>
 
-
-#include <Defines.h>
-#include <Log.h>
+#include "Defines.h"
+#include "Log.h"
+#include "CommThread.h"
+#include "NotifyThread.h"
 
 void deamonize();
 
-void initLog();
-
-void log(LogLevel loglevel, const char* msg);
+void signalHandler(int sig);
 
 int main() {
 
 	// deamonize this process
 	deamonize();
 
+	/* Change the current working directory */
+	if ((chdir("/tmp/")) < 0) {
+		log(ERROR, "chdir() failed");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Close out the standard file descriptors */
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	/* Daemon-specific initialization goes here */
+	signal(SIGCHLD, SIG_IGN); /* ignore child */
+	signal(SIGTSTP, SIG_IGN); /* ignore tty signals */
+	signal(SIGTTOU, SIG_IGN); /*  --- " --- */
+	signal(SIGTTIN, SIG_IGN); /*  --- " --- */
+	signal(SIGHUP, signalHandler); /* catch hangup signal */
+	signal(SIGTERM, signalHandler); /* catch kill signal */
+
+	CommThread communication_thread;
+	NotifyThread notification_thread;
+
+	communication_thread.run();
+	notification_thread.run();
+
+	communication_thread.join();
+	notification_thread.join();
+
+	log(INFO, "Program terminates.");
+	closelog();
+	exit(EXIT_SUCCESS);
 }
 
 void deamonize() {
@@ -59,5 +87,18 @@ void deamonize() {
 	if (sid < 0) {
 		log(ERROR, "setsid() failed");
 		exit(EXIT_FAILURE);
+	}
+}
+
+void signalHandler(int sig) {
+	switch (sig) {
+	case SIGHUP:
+		log(INFO, "hangup signal detected");
+		break;
+	case SIGTERM:
+		log(INFO, "terminate signal detected");
+		closelog();
+		exit(0);
+		break;
 	}
 }
