a7a175d91581b4a8311e7daa5f1d433ad791748d
Thomas Ruess <rui.nozeros@posteo.at>
2019-05-29 23:46:23 +0200

    dont override existing config file

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,8 +9,13 @@ if [ ! -d $config_dir ]; then
     mkdir $config_dir
 fi
 
-cp config $config_dir/
-cp img/coffee.png $config_dir/
+if [ ! -f "$config_dir/config" ]; then
+    cp config $config_dir/
+fi
+
+if [ ! -f "$config_dir/coffee.png" ]; then
+    cp img/coffee.png $config_dir/
+fi
 
 echo "start deamon: eyeronic --config=~/.config/eyeronic/config --start"
 
